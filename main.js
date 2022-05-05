4114288e941200c7a87acda2c5c0a664c0f2b967
matsumotojunki <104050112+matsumotojunki@users.noreply.github.com>
2022-05-05 17:58:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//ハンバーガーメニューここから
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+//faqここから
 //アコーディオンをクリックした時の動作
 $('.title').on('click', function() {//タイトル要素をクリックしたら
 	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
@@ -10,13 +23,33 @@ $('.title').on('click', function() {//タイトル要素をクリックしたら
 	}
 });
 
-//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
-$(window).on('load', function(){
-	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
-	$(".open").each(function(index, element){	//openクラスを取得
-		var Title =$(element).children('.title');	//openクラスの子要素のtitleクラスを取得
-		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
-		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
-		$(Box).slideDown(500);					//アコーディオンを開く
-	});
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
 });
+
+
+    //---リンク先までスクロール
+    $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+        var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+        var pos = $(elmHash).offset().top; //idの上部の距離を取得
+        $('body,html').animate({
+            scrollTop: pos
+        }, 300); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+        return false;
+    });
